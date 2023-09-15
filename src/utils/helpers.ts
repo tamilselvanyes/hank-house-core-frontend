90185795df36f5e0b1e1a2b013bf23bc0e35a933
tamilselvanyes <tamilselvanyes@gmail.com>
2023-09-16 00:42:53 +0530

    chore: created helpers and constants file

---
+++ src/utils/helpers.ts
@@ -0,0 +1,11 @@
+import { characters } from "./constant";
+
+export const generate_random_string = (length: number): string => {
+  let result = " ";
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+};
